6ea1bd77da7e10976b32f2ac887d7c907faef38d
Alexandr Varugin <otz-job@mail.ru>
2019-01-15 15:39:21 +0400

    practically-focused assignment №3

---
+++ controllers/CategoryController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: OTZ_VAV
+ * Date: 15.01.2019
+ * Time: 9:47
+ */
+
+namespace app\controllers;
+use app\models\Good;
+use yii\web\Controller;
+use Yii;
+
+class CategoryController extends Controller
+{
+    public function actionIndex() {
+        $goods = new Good();
+        $goods = $goods->getAllGoods();//Все данные из таблицы попадают в контроллер
+        return $this->render('index', compact('goods'));//Передача данных во вьюшку
+    }
+
+    public function actionView($id)
+    {
+        $goods = new Good();
+        $goods = $goods->getGoodsCategories($id);
+        return $this->render('view', compact('goods'));
+    }
+
+    public function actionSearch()
+    {
+        $search = Yii::$app->request->get('search');//Считываем данные из переменной полученной с помощью get запроса
+        $search = strip_tags($search);
+        $search = htmlspecialchars($search);
+        $goods = new Good();
+        $goods = $goods->getSearchResults($search);
+        return $this->render('search', compact('goods', 'search'));
+    }
+
+}
